f6f3d2a5b427976da8454cf64f93b9300bde2580
sbrahma2302 <91287390+sbrahma2302@users.noreply.github.com>
2021-09-23 15:45:36 -0400

    Add files via upload

---
+++ src/theme/themes.js
@@ -0,0 +1,12 @@
+export const light = {
+    body: '#FFF',
+    font: '#363537',
+    heading:'#455b9e',
+    bgTh:'#99c4b2',
+}
+export const dark = {
+    font: '#FAFAFA',
+    body: '#2b2c30f5',
+    heading:'#91e0cf',
+    bgTh:'#956e6e',
+}
