6e351b682a5b9811e50ff060b93c69094d7b178f
sbrahma2302 <91287390+sbrahma2302@users.noreply.github.com>
2021-09-27 21:38:21 -0400

    Add files via upload

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle} from "styled-components"
+import { createGlobalStyle } from "styled-components"
 export const GlobalStyles = createGlobalStyle`
     body {
         transition: all 1.0s ease;
